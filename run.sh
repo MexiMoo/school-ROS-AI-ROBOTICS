a163f783d1b7b661657ff99bfb4b89692926a30b
Mexi <100799507+MexiMoo@users.noreply.github.com>
2026-06-16 16:37:48 +0200

    Update run.sh
    Updater weggehaald, word ander bestand

--- run.sh
+++ run.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-set -e
-
-REPO_URL="https://github.com/MexiMoo/school-ROS-AI-ROBOTICS.git"
-BRANCH="main"
-
-echo "Syncing latest GitHub version..."
-
-# workspace opzetten
-mkdir -p workspace/src
-
-cd workspace/src
-
-if [ ! -d "repo" ]; then
-    echo "Cloning repo..."
-    git clone -b $BRANCH $REPO_URL repo
-else
-    echo "Pulling latest changes..."
-    cd repo
-    git pull origin $BRANCH
-    cd ..
-fi
+echo test
