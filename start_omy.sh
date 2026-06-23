5b867a930d4bf9a9ca94a909f7a9f1a18b488d8c
smelker324-hub <smelker324@gmail.com>
2026-06-23 12:34:58 +0200

    Update start_omy.sh

--- start_omy.sh
+++ start_omy.sh
@@ -1,42 +1,57 @@
 #!/bin/bash
+
 set -e
 
 ROBOT_HOST="root@omy-SNPR44B1011.local"
 
-echo "=== OMY Volledige Startup ==="
+echo "OMY_f3m setup"
 
-#Docker opzetten op de ROBOT PC
-echo "[1/2] Robot PC: Open Manipulator container starten..."
-ssh "$ROBOT_HOST" "cd /data/docker/open_manipulator && docker compose up -d"
-echo "      Wachten tot robot bringup actief is..."
-sleep 10
+echo "camera setup"
+cd ~/open_manipulator/docker
 
-# Docker opzetten voor de AI op de LAPTOP
-echo "[2/2] Laptop: Physical AI Tools container starten..."
-cd ~/physical_ai_tools/docker
-
-# Container starten
 ./container.sh start
-
-# Camera's en AI server starten binnenin de container
 ./container.sh enter bash -c "
+
   source /opt/ros/jazzy/setup.bash
-  source ~/ros2_ws/install/setup.bash 2>/dev/null || true
 
-  echo 'Camera nodes starten...'
   ros2 launch realsense2_camera rs_multi_camera_launch.py \
-      camera_name1:=cam_wrist serial_no1:=\"'_230322274265'\" device_type1:=d4 \
-      camera_name2:=cam_top serial_no2:=\"'_233522071994'\" device_type2:=d4 &
-  sleep 3
-
-  ros2 launch realsense2_camera rs_launch.py \
-      camera_name:=cam_side serial_no:=\"'_837212070319'\" device_type:=d4 &
-  sleep 3
-
-  echo 'Physical AI Server starten...'
-  ros2 launch physical_ai_server physical_ai_server_bringup.launch.py &
+    camera_name1:=cam_wrist serial_no1:=\"'_230322274265'\" \
+    depth_module.color_profile1:=480,270,30 \
+    depth_module.depth_profile1:=480,270,30 \
+    device_type1:=d4 \
+    camera_name2:=cam_top serial_no2:=\"'_837212070319'\" \
+    depth_module.color_profile2:=480,270,30 \
+    depth_module.depth_profile2:=480,270,30 \
+    device_type2:=d4 &
 
   wait
 "
 
-echo "=== OMY volledig opgestart! ==="
+sleep 5
+
+echo "physical ai tools server opzetten"
+cd ~/physical_ai_tools/docker
+
+./container.sh start
+
+./container.sh enter bash -c "
+  source /opt/ros/jazzy/setup.bash
+
+  source ~/ros2_ws/install/setup.bash 2>/dev/null || true
+
+  ai_server &
+
+  wait
+"
+
+sleep 5
+echo "Open manipulator docker opzetten"
+ssh "$ROBOT_HOST" "cd /data/docker/open_manipulator && ./docker/container.sh start"
+
+ssh "$ROBOT_HOST" "./docker/container.sh enter bash -c 'source /opt/ros/jazzy/setup.bash && ros2 launch open_manipulator_bringup omy_ai.launch.py'"
+
+echo "Robotcel opgestart"
+echo "WebUI beschikbaar op: http://localhost/"
+# Herinnering waar de WebUI te vinden is
+echo "Debugging:            http://localhost:8080"
+# Poort 8080 voor debugging en alle beschikbare nodes
