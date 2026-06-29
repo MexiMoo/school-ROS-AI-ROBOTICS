3b9bae7dc27558d4628175cec0441fd5f2578e44
smelker324-hub <smelker324@gmail.com>
2026-06-29 15:03:38 +0200

    Update auto-update.sh

--- auto-update.sh
+++ auto-update.sh
@@ -38,6 +38,11 @@ else
 fi
 chmod +x run.sh
 
+docker cp /home/student/school-ROS-AI-ROBOTICS/HMI/config.py physical_ai_server:/root/HMI/config.py
+docker cp /home/student/school-ROS-AI-ROBOTICS/HMI/hmi_window.py physical_ai_server:/root/HMI/hmi_window.py
+docker cp /home/student/school-ROS-AI-ROBOTICS/HMI/main.py physical_ai_server:/root/HMI/main.py
+docker cp /home/student/school-ROS-AI-ROBOTICS/HMI/ros_interface.py physical_ai_server:/root/HMI/ros_interface.py
+
 echo
 echo -e "${GREEN}Done!${NO_FORMAT} Starting auto-run script..."
 echo
