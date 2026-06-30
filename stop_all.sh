37d9164ed3ff0daed62259ab42048e4e0468569a
smelker324-hub <smelker324@gmail.com>
2026-06-30 16:42:48 +0200

    Update stop_all.sh

--- stop_all.sh
+++ stop_all.sh
@@ -7,8 +7,8 @@ GREEN="\033[38;5;47m"
 
 echo "ROS processen stoppen..."
 
-docker exec open_manipulator pkill -9 -f ros2 2>/dev/null || true
-docker exec physical_ai_server pkill -9 -f ros2 2>/dev/null || true
+sudo docker exec open_manipulator pkill -9 -f ros2 2>/dev/null || true
+sudo docker exec physical_ai_server pkill -9 -f ros2 2>/dev/null || true
 
 ssh "$ROBOT_HOST" 'docker exec open_manipulator pkill -9 -f ros2 2>/dev/null || true'
 ssh "$ROBOT_HOST" 'reboot'
